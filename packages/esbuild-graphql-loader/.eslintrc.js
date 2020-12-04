03ad89d013d942d3eacb6fae8a43b9cbc8cdaee9
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-12-04 14:53:31 +0800

    Update linter rule

---
+++ packages/esbuild-graphql-loader/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": [
+    'lxsmnsyc/typescript',
+  ],
+  "parserOptions": {
+    "project": "./tsconfig.eslint.json",
+    "tsconfigRootDir": __dirname,
+  },
+  "rules": {
+  },
+};
+
